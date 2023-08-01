a2a0abd4944e23f31c423e6a278f1df1adc47474
Jason Tay <jtay0034@student.monash.edu>
2023-08-01 19:00:47 +1000

    Made database scripts for user and domain

---
+++ app/db_domain.js
@@ -0,0 +1,39 @@
+const { MongoClient } = require('mongodb');
+
+const userName = "FIT4002";
+const password = "monashFIT4002";
+const domainCollectionName = "domain";
+
+const uri = "mongodb+srv://" + userName + ":" + password + "@fit4002-team04.ep77oco.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a new MongoClient
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function getDomains() {
+    var domains = null
+    try {
+        // Find all domains
+        await client.connect();
+        domains = await client.db("fit4002-project")
+        .collection(domainCollectionName).find();
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+    return domains;
+}
+
+async function addDomain(name) {
+    try {
+        // Add new domain
+        await client.connect();
+        await client.db("fit4002-project")
+        .collection(domainCollectionName).insertOne({domainName: name});
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
---
+++ app/db_user.js
@@ -0,0 +1,39 @@
+const { MongoClient } = require('mongodb');
+
+const userName = "FIT4002";
+const password = "monashFIT4002";
+const userCollectionName = "user";
+
+const uri = "mongodb+srv://" + userName + ":" + password + "@fit4002-team04.ep77oco.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a new MongoClient
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function getUser(username, pwd) {
+    var user = null
+    try {
+        // Find user by username
+        await client.connect();
+        user = await client.db("fit4002-project")
+        .collection(userCollectionName).findOne({userName: username, password: pwd});
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+    return user;
+}
+
+async function signUpUser(username, pwd) {
+    try {
+        // Find user by username
+        await client.connect();
+        user = await client.db("fit4002-project")
+        .collection(userCollectionName).insertOne({userName: username, password: pwd});
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
