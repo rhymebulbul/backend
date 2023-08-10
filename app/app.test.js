0fc22868649dfe484055770a3dc0acd8daac9c1e
Rhyme Bulbul <rbul0005@student.monash.edu>
2023-08-10 20:32:17 +1000

    Update unittests for app.js

--- app/app.test.js
+++ app/app.test.js
@@ -6,6 +6,7 @@ const cookieSession = require("cookie-session")
 const { USERNAME,PASSWORD,DATABASE,CLUSTER } = require('./config/db.config.js')
 const db = require("./models/index.js")
 const session = require('express-session');
+const cheerio = require('cheerio');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('./app'); // Assuming app setup is in 'app.js'
@@ -20,6 +21,27 @@ describe('/domain Route', () => {
         .end((err, res) => {
           expect(res).to.have.status(404);
           expect(res.text).to.include('domain');
+          done();
+        });
+    });
+  });
+
+
+describe('/domain Route', () => {
+    it('should respond with a message containing the phrase "Your Domain Selection is"', (done) => {
+      chai.request('http://localhost:3000')
+        .get('/domain')
+        .end((err, res) => {
+          //expect(res).to.have.status(404);
+          
+          // Parse the HTML using cheerio
+          const $ = cheerio.load(res.text);
+          
+          // Check for the presence of the desired text within the HTML
+          const containsText = $('body').text().includes('Domain');
+          console.log(containsText)
+          expect(containsText).to.be.false;
+          
           done();
         });
     });
