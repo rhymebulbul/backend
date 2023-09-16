6e914807d28f1007b651b4a3d36079bae0e08685
YichengPeng2209 <ypen0032@student.monash.edu>
2023-09-17 01:07:23 +1000

    add delete factors functions

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -31,10 +31,10 @@ exports.moderatorBoard = (req, res) => {
 
 exports.addDomain = async (req, res) => {
   const userIdString = req.userId;
-  const factorName = req.body.factorName;
+  const domainName = req.body.domainName;
   const userId = new ObjectId(userIdString);
 
-  if (!factorName) {
+  if (!domainName) {
     return res.status(400).send({ message: "Domain name is required." });
   }
 
@@ -46,7 +46,7 @@ exports.addDomain = async (req, res) => {
 
     // Define the update statement
     const update = {
-      $addToSet: { userDomains: { "factorName": factorName } }
+      $addToSet: { userDomains: { "domainName": domainName } }
     };
 
     // Update the user
@@ -65,6 +65,46 @@ exports.addDomain = async (req, res) => {
     res.status(500).send({ message: "There was a problem adding the domain: " + error.message });
   }
 }
+
+
+exports.deleteDomain = async (req, res) => {
+  const userIdString = req.userId;
+  const domainName = req.body.domainName;
+  const userId = new ObjectId(userIdString);
+
+  if (!domainName) {
+    return res.status(400).send({ message: "Domain name is required." });
+  }
+
+  try {
+    await client.connect();
+
+    // Define the query for the user you want to update
+    const query = { _id: userId };
+
+    // Define the update statement to remove the domain from the user's array
+    const update = {
+      $pull: { userDomains: { "domainName": domainName } }
+    };
+
+    // Update the user's document
+    const result = await client.db("fit4002-project")
+      .collection("users")
+      .updateOne(query, update);
+
+    // Check if a document was modified
+    if (result.modifiedCount === 0) {
+      throw new Error("User not found or domain doesn't exist for user.");
+    }
+
+    res.status(200).send({ message: "Domain removed from user successfully." });
+
+  } catch (error) {
+    res.status(500).send({ message: "There was a problem deleting the domain: " + error.message });
+  }
+}
+
+
 
 exports.getUserDomains = async (req, res) => {
   const userIdString = req.userId;
@@ -127,6 +167,43 @@ exports.addInternalFactor = async (req, res) => {
     res.status(500).send({ message: "There was a problem adding the factor: " + error.message });
   }
 }
+
+exports.deleteInternalFactor = async (req, res) => {
+  const userIdString = req.userId;
+  const factorName = req.body.factorName;
+  const userId = new ObjectId(userIdString);
+  if (!factorName) {
+    return res.status(400).send({ message: "Internal human factor name is required." });
+  }
+
+  try {
+    await client.connect();
+
+    // Define the query for the user you want to update
+    const query = { _id: userId };
+
+    // Define the update statement
+    const update = {
+      $pull: { userInternalFactors: { "factorName": factorName } }
+    };
+
+    // Update the user
+    const result = await client.db("fit4002-project")
+      .collection("users")
+      .updateOne(query, update);
+
+    // Check if a document was modified
+    if (result.modifiedCount === 0) {
+      throw new Error("User not found or Factor doesn't exist for user.");
+    }
+
+    res.status(200).send({ message: "Factor removed from user successfully." });
+
+  } catch (error) {
+    res.status(500).send({ message: "There was a problem removing the factor: " + error.message });
+  }
+}
+
 
 exports.getInternalFactors = async (req, res) => {
   const userIdString = req.userId;
@@ -194,6 +271,42 @@ exports.addExternalFactor = async (req, res) => {
   }
 }
 
+exports.deleteExternalFactor = async (req, res) => {
+  const userIdString = req.userId;
+  const factorName = req.body.factorName;
+  const userId = new ObjectId(userIdString);
+
+  if (!factorName) {
+    return res.status(400).send({ message: "External human factor name is required." });
+  }
+
+  try {
+    await client.connect();
+
+    // Define the query for the user you want to update
+    const query = { _id: userId };
+
+    // Define the update statement
+    const update = {
+      $pull: { userExternalFactors: { "factorName": factorName } }
+    };
+
+    // Update the user
+    const result = await client.db("fit4002-project")
+      .collection("users")
+      .updateOne(query, update);
+
+    // Check if a document was modified
+    if (result.modifiedCount === 0) {
+      throw new Error("User not found or external human factor doesn't exist for user.");
+    }
+
+    res.status(200).send({ message: "External human factor removed from user successfully." });
+
+  } catch (error) {
+    res.status(500).send({ message: "There was a problem removing the external human factor: " + error.message });
+  }
+}
 
 
 
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -11,12 +11,15 @@ module.exports = function (app) {
   });
 
   app.post("/api/user/addDomain", authJwt.verifyToken, controller.addDomain);
+  app.post("/api/user/deleteDomain", authJwt.verifyToken, controller.deleteDomain);
   app.get("/api/user/getDomains", authJwt.verifyToken, controller.getUserDomains);
 
   app.post("/api/user/addInternalFactor", authJwt.verifyToken, controller.addInternalFactor);
+  app.post("/api/user/deleteInternalFactor", authJwt.verifyToken, controller.deleteInternalFactor);
   app.get("/api/user/getInternalFactors", authJwt.verifyToken, controller.getInternalFactors);
 
   app.post("/api/user/addExternalFactor", authJwt.verifyToken, controller.addExternalFactor);
+  app.post("/api/user/deleteExternalFactor", authJwt.verifyToken, controller.deleteExternalFactor);
   app.get("/api/user/getExternalFactors", authJwt.verifyToken, controller.getExternalFactors);
 
 
